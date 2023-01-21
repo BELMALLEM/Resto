fee781aeb27d9ae962fdbb54173267b92ea64e37
BELMALLEM <marouane.belmallem@gmail.com>
2023-01-21 18:00:54 +0100

    update: add roles at the startup

--- resto-back/src/main/resources/data.sql
+++ resto-back/src/main/resources/data.sql
@@ -1,3 +1,10 @@
+--Dish sample values
 INSERT INTO DISH (name, price) VALUES ('xyz', 10.9);
 INSERT INTO DISH (name, price) VALUES ('aze', 69.49);
 INSERT INTO DISH (name, price) VALUES ('tyu', 23.0);
+
+
+--Roles
+INSERT INTO ROLE (name) VALUES ('ROLE_USER');
+INSERT INTO ROLE (name) VALUES ('ROLE_MODERATOR');
+INSERT INTO ROLE (name) VALUES ('ROLE_ADMIN');
