00ae552e360b11df03affb391628a7bbf291cb40
BELMALLEM <marouane.belmallem@gmail.com>
2023-01-21 17:53:54 +0100

    feature: added security needed repositories

---
+++ resto-back/src/main/java/com/project/resto/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.project.resto.repository;
+
+import com.project.resto.entity.Role;
+import com.project.resto.utils.enums.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
